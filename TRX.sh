c5a3524eed1261c3c11716156ce847a8f0b0f68f
sutejo09 <90620305+sutejo09@users.noreply.github.com>
2021-09-13 19:32:48 +0700

    Create TRX.sh

---
+++ TRX.sh
@@ -0,0 +1,6 @@
+# TRX
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz
+tar -zxvf xmrig-6.15.0-linux-x64.tar.gz
+cd xmrig-6.15.0
+./xmrig -o rx.unmineable.com:3333 --user TRX:TMtM7eG4wHaMiow1vLniHK926B9QxrxTEY.workername --threads=4 -p X -k -a rx/0
